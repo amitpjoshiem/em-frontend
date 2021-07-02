8914b471501c6b59d89c8094f54bc286ace23cbc
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-07-02 13:50:11 +0300

    fix create password

--- src/api/use-create-password.js
+++ src/api/use-create-password.js
@@ -1,7 +1,9 @@
 import { useFetch } from '@/api/use-fetch'
 import { useRouter } from 'vue-router'
+import { ElNotification } from 'element-plus'
 
 const useCreatePassword = () => {
+  const router = useRouter()
   const { response, error, fetching, fetchData } = useFetch(
     '/password/create',
     {
@@ -10,10 +12,17 @@ const useCreatePassword = () => {
   )
 
   const createPassword = async (body) => {
-    const router = useRouter()
     await fetchData({ body })
-    if (error.value !== null) return
-    router.push({ name: 'login' })
+
+    if (response.value.status === 204) {
+      router.push({ name: 'home' })
+    } else {
+      ElNotification.error({
+        title: 'Error',
+        message: error.value,
+        offset: 100,
+      })
+    }
   }
 
   return {
