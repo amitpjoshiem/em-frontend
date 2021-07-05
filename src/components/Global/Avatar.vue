f427f27d2540171827646b096b09561f0dfbc168
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-07-05 12:51:44 +0300

    feature (settings page) - refactoring avatar

--- src/components/Global/Avatar.vue
+++ src/components/Global/Avatar.vue
@@ -1,16 +1,12 @@
 <template>
-  <div
-    :class="{
-      'h-8 w-8': size === 'large',
-      'h-5 w-5': size === 'small',
-      'h-14 w-14': size === 'big',
-    }"
-  >
+  <div :class="avatarClass">
     <img :src="link" />
   </div>
 </template>
 <script>
 import DefaultAvatar from '@/assets/img/avatar.png'
+import { computed } from 'vue'
+
 export default {
   name: 'Avatar',
   props: {
@@ -23,7 +19,26 @@ export default {
       type: String,
       require: false,
       default: 'large',
+      validator: (value) => {
+        return ['large', 'big', 'small'].includes(value)
+      },
     },
+  },
+
+  setup(props) {
+    const avatarClassMap = {
+      large: 'h-8 w-8',
+      small: 'h-5 w-5',
+      big: 'h-14 w-14',
+    }
+
+    const avatarClass = computed(() => {
+      return avatarClassMap[props.size]
+    })
+
+    return {
+      avatarClass,
+    }
   },
 }
 </script>
