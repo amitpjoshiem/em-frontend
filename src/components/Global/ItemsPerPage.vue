abdce7100441954f60b67104d9f750b7c9a21f78
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-07-30 13:38:52 +0300

    feature (list of households logic)

--- src/components/Global/ItemsPerPage.vue
+++ src/components/Global/ItemsPerPage.vue
@@ -2,7 +2,6 @@
   <div class="mr-2.5 flex items-center">
     <span class="text-xss text-text-gray mr-1.5">Items per page</span>
     <SwdSelect
-      class="w-3/12"
       :options="actionsOptions"
       size="small"
       init-value="6"
