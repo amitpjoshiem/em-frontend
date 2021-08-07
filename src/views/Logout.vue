296dd72ddf27694304b24e38d5a464c46ba69743
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-08-07 18:31:29 +0300

    feature (logout refactoring)

--- src/views/Logout.vue
+++ src/views/Logout.vue
@@ -1,25 +1,38 @@
-<template>
-  <div>Logout...</div>
-</template>
-
 <script>
+import { defineComponent, watch } from 'vue'
+import { ElLoading } from 'element-plus'
 import { onMounted } from 'vue'
 import { useLogout } from '@/api/authentication/use-logout'
 
-export default {
-  name: 'Logout',
+export default defineComponent({
   setup() {
     const { error, fetching, logout } = useLogout()
 
+    let loadingInstance
+
     onMounted(() => {
-      logout()
+      loadingInstance = ElLoading.service({
+        lock: true,
+        text: 'Loading',
+        spinner: 'el-icon-loading',
+        background: 'rgba(0, 0, 0, 0.7)',
+      })
+      setTimeout(() => {
+        logout()
+      }, 1000)
+    })
+
+    watch(fetching, (newValue) => {
+      if (newValue === false) {
+        loadingInstance.close()
+      }
     })
 
     return {
+      loadingInstance,
       error,
       fetching,
-      logout,
     }
   },
-}
+})
 </script>
