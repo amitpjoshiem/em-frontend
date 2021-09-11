542106fc6756af1c56bfdf4d9460789d0df5709c
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-09-11 15:34:21 +0300

    refactoring (refactoring v3) - removing components

--- src/main.js
+++ src/main.js
@@ -12,22 +12,14 @@ import InlineSvg from 'vue-inline-svg'
 import Button from '@/components/Global/Button/Button.vue'
 import CircularProgress from '@/components/Global/CircularProgress.vue'
 import LinearProgress from '@/components/Global/LinearProgress.vue'
-import ActionBtn from '@/components/Global/Button/ActionBtn.vue'
 import ShareBtn from '@/components/Global/Button/ShareBtn.vue'
 import Avatar from '@/components/Global/Avatar.vue'
 import TypeUserLabel from '@/components/Global/TypeUserLabel.vue'
 import Select from '@/components/Global/Form/Select.vue'
 import SwdSelect from '@/components/Global/Form/SwdSelect.vue'
-import SwdSelectForm from '@/components/Global/Form/SwdSelectForm.vue'
 import ItemsPerPage from '@/components/Global/ItemsPerPage.vue'
 import DropDown from '@/components/Global/DropDown.vue'
-import Checkbox from '@/components/Global/Сheckbox.vue'
-import Label from '@/components/Global/Label.vue'
-import TextArea from '@/components/Global/TextArea.vue'
-import Radio from '@/components/Global/Radio.vue'
-import RadioBoolean from '@/components/Global/RadioBoolean.vue'
 import DatePicker from '@/components/Global/DatePicker.vue'
-import NextButton from '@/components/Global/Button/NextButton.vue'
 import BackButton from '@/components/Global/Button/BackButton.vue'
 import Dialog from '@/components/Global/Dialog.vue'
 import Pagination from '@/components/Global/Pagination.vue'
@@ -70,55 +62,47 @@ app.use(store)
 
 // Global Component
 const components = [
-  ElProgress,
   Button,
   InlineSvg,
   CircularProgress,
   LinearProgress,
-  ActionBtn,
   ShareBtn,
   Avatar,
   TypeUserLabel,
   Select,
   ItemsPerPage,
+  DropDown,
+  DatePicker,
+  BackButton,
+  Dialog,
+  Pagination,
+  SwdSelect,
+  SubHeader,
+  PlugForField,
+  MemberActions,
+  ElForm,
+  ElFormItem,
+  ElAutocomplete,
+  ElProgress,
+  ElSkeleton,
+  ElOption,
+  ElRadioGroup,
+  ElPopper,
+  ElSelect,
+  ElButton,
+  ElPagination,
+  ElSwitch,
+  ElDialog,
+  ElPopover,
+  ElCheckbox,
+  ElRadio,
+  ElInput,
+  ElDatePicker,
   ElSteps,
   ElStep,
   ElDropdown,
   ElDropdownMenu,
   ElDropdownItem,
-  DropDown,
-  Checkbox,
-  ElCheckbox,
-  ElRadio,
-  Label,
-  TextArea,
-  ElInput,
-  Radio,
-  ElDatePicker,
-  DatePicker,
-  ElPopover,
-  NextButton,
-  BackButton,
-  ElSwitch,
-  ElDialog,
-  Dialog,
-  ElButton,
-  ElPagination,
-  Pagination,
-  ElRadioGroup,
-  ElPopper,
-  RadioBoolean,
-  ElSelect,
-  SwdSelect,
-  SwdSelectForm,
-  ElOption,
-  SubHeader,
-  ElSkeleton,
-  PlugForField,
-  ElForm,
-  ElFormItem,
-  ElAutocomplete,
-  MemberActions,
 ]
 
 components.forEach((component) => {
