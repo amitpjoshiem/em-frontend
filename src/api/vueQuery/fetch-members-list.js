6a3d7e6a563a1418bac2cfd73d34d866199f673c
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-09-23 16:59:52 +0300

    refactoring (refactoring List of Households)

--- src/api/vueQuery/fetch-members-list.js
+++ src/api/vueQuery/fetch-members-list.js
@@ -1,13 +1,18 @@
 import { fetcher } from '@/api/fetcher/fetcher'
 
 function fetchMembersList({ queryKey }) {
-  const type = queryKey[1].reactiveType
+  const typeRaw = queryKey[1].reactiveType
   const limit = queryKey[1].reactiveLimit
   const page = queryKey[1].reactivePage ? queryKey[1].reactivePage.page : ''
+  let search = undefined
+
+  if (typeRaw !== 'all') {
+    search = `type:${typeRaw}`
+  }
 
   return fetcher({
     url: `/members`,
-    options: { method: 'GET', searchParams: { type, limit, page } },
+    options: { method: 'GET', searchParams: { search, limit, page } },
   })
 }
 
