1c198385028ae50f5814bfe3210fad7fd0a1e706
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-09-27 18:11:31 +0300

    feature (native select) - minor edit

--- src/components/Global/SwdNativeSelect.vue
+++ src/components/Global/SwdNativeSelect.vue
@@ -76,11 +76,9 @@ export default {
 <style>
 .styled-select {
   height: 30px;
-  /* -webkit-appearance: none; */
   -webkit-appearance: none;
   -moz-appearance: none;
   appearance: none;
-
   background: transparent;
   background-image: url("data:image/svg+xml;utf8,<svg fill='black' height='24' viewBox='0 0 24 24' width='24' xmlns='http://www.w3.org/2000/svg'><path d='M7 10l5 5 5-5z'/><path d='M0 0h24v24H0z' fill='none'/></svg>");
   background-repeat: no-repeat;
