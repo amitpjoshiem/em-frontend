c73d3ce76f5769f7738ab31375ca1612c2a21b46
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-10-01 12:53:12 +0300

    feature (Activity)

---
+++ src/components/Activity/ActivityContent.vue
@@ -0,0 +1,83 @@
+<template>
+  <div v-if="!isFetching" class="border rounded-lg p-5">
+    <span class="text-main text-smm font-semibold">Your Activity</span>
+    <div class="mt-5">
+      <el-timeline v-for="(elem, index) in data.data" :key="index">
+        <div class="mb-6 text-gray03 font-semibold">
+          <TitleDayActivity :day="elem.day" />
+        </div>
+        <el-timeline-item
+          v-for="item in elem.events"
+          :key="item.timestamp"
+          center
+          :timestamp="item.timestamp"
+          placement="top"
+          color="#66B6FF"
+        >
+          <div v-html="item.content" />
+        </el-timeline-item>
+      </el-timeline>
+    </div>
+  </div>
+  <el-skeleton v-else :rows="5" animated class="p-5" />
+</template>
+<script>
+import { ref } from 'vue'
+import { useFetchActivities } from '@/api/use-fetch-activities.js'
+import TitleDayActivity from './TitleDayActivity.vue'
+
+export default {
+  name: 'ActivityContent',
+  components: {
+    TitleDayActivity,
+  },
+  setup() {
+    const activeName = ref('')
+    const { data, error, isFetching, refetch } = useFetchActivities()
+
+    return {
+      activeName,
+      data,
+      error,
+      isFetching,
+      refetch,
+    }
+  },
+}
+</script>
+
+<style>
+.el-timeline-item__content {
+  background-color: #f2f5fa;
+  border-radius: 10px;
+}
+
+.el-timeline-item__content p {
+  padding-left: 18px;
+  padding-bottom: 12px;
+  padding-top: 12px;
+  font-size: 12px;
+}
+
+.el-timeline-item__content p a {
+  color: #424450;
+  font-weight: 600;
+}
+.el-timeline-item__wrapper {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.el-timeline-item__timestamp.is-top {
+  width: 10%;
+  margin-bottom: 0;
+  color: #424450;
+  font-size: 12px;
+  font-weight: 600;
+}
+
+.el-timeline-item__content {
+  width: 90%;
+}
+</style>
--- src/views/Activity.vue
+++ src/views/Activity.vue
@@ -1,94 +1,16 @@
 <template>
   <div class="p-5">
     <SwdSubHeader title="Activity" />
-    <div v-if="!isFetching" class="border rounded-lg p-5">
-      <div>
-        <span class="text-main text-smm font-semibold">Your Activity</span>
-        <div class="mt-5">
-          <el-timeline v-for="(elem, index) in getData" :key="index">
-            <div class="mb-6 text-gray03 font-semibold">
-              {{ elem.day }}
-            </div>
-            <el-timeline-item
-              v-for="item in elem.content"
-              :key="item.timestamp"
-              center
-              :timestamp="item.timestamp"
-              placement="top"
-              color="#66B6FF"
-            >
-              <div v-html="item.content" />
-            </el-timeline-item>
-          </el-timeline>
-        </div>
-      </div>
-    </div>
-    <el-skeleton v-else :rows="5" animated class="p-5" />
+    <ActivityContent />
   </div>
 </template>
 <script>
-import { ref, computed } from 'vue'
-import { useFetchActivities } from '@/api/use-fetch-activities.js'
-import dayjs from 'dayjs'
+import ActivityContent from '@/components/Activity/ActivityContent.vue'
 
 export default {
   name: 'Activity',
-  setup() {
-    const activeName = ref('')
-    const { data, error, isFetching, refetch } = useFetchActivities()
-
-    const getData = computed(() => {
-      const res = []
-      Object.keys(data.value.data).forEach((key) => {
-        res.push({ day: dayjs(key).calendar(null), content: data.value.data[key] })
-      })
-      return res
-    })
-
-    return {
-      activeName,
-      data,
-      error,
-      isFetching,
-      refetch,
-      getData,
-    }
+  components: {
+    ActivityContent,
   },
 }
 </script>
-
-<style>
-.el-timeline-item__content {
-  background-color: #f2f5fa;
-  border-radius: 10px;
-}
-
-.el-timeline-item__content p {
-  padding-left: 18px;
-  padding-bottom: 12px;
-  padding-top: 12px;
-  font-size: 12px;
-}
-
-.el-timeline-item__content p a {
-  color: #424450;
-  font-weight: 600;
-}
-.el-timeline-item__wrapper {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-}
-
-.el-timeline-item__timestamp.is-top {
-  width: 10%;
-  margin-bottom: 0;
-  color: #424450;
-  font-size: 12px;
-  font-weight: 600;
-}
-
-.el-timeline-item__content {
-  width: 90%;
-}
-</style>
