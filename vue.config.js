1ee0f8b53091deddceb86add58d0a13dfe465ed8
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-10-22 20:13:19 +0300

    fix (pwa)

--- vue.config.js
+++ vue.config.js
@@ -34,8 +34,8 @@ module.exports = {
     },
 
     workboxOptions: {
-      swSrc: './src/registerServiceWorker.js',
-      // skipWaiting: true,
+      // swSrc: './src/registerServiceWorker.js',
+      skipWaiting: true,
     },
 
     iconPaths: {
