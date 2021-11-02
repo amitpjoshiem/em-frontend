7bea35be0f3b4f5efd9b731535080ecc4b6bda3a
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-11-02 18:46:56 +0200

    minor edit

--- src/api/vueQuery/create-stress-test.js
+++ src/api/vueQuery/create-stress-test.js
@@ -1,7 +1,6 @@
 import { fetcher } from '@/api/fetcher/fetcher'
 
 function createStressTest({ id, data }) {
-  console.log('data - ', data)
   return fetcher({
     url: `/members/stress_test/${id}`,
     data,
