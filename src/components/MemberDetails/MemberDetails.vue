62f3494615befbd899707c6dc8928f1f2e0ca6d9
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-11-09 15:40:56 +0200

    feature (risk level block)

--- src/components/MemberDetails/MemberDetails.vue
+++ src/components/MemberDetails/MemberDetails.vue
@@ -1,16 +1,18 @@
 <template>
   <div class="p-5">
     <SwdSubHeader :title="getTitle" />
-    <div class="flex my-5">
-      <WidgetMemberDetails v-if="!isLoadingProspectDetails" :user="data" @updateMemberInfo="updateMemberInfo" />
-      <el-skeleton v-else :rows="6" animated class="w-8/24 pr-5 h-[264px]" />
-      <AssetsAllocation />
+    <div v-if="!isLoadingProspectDetails" class="flex my-5">
+      <WidgetMemberDetails :user="member" class="w-8/24" @updateMemberInfo="updateMemberInfo" />
+      <AssetsAllocation class="w-10/24" />
+      <WidgetOther class="w-6/24" :other="other" />
     </div>
+    <el-skeleton v-else :rows="6" animated class="pr-5 h-[264px]" />
+
     <div class="flex mb-5">
       <WidgetPastStress />
       <RetirementIncomePlan />
     </div>
-    <OpportunityTable class="mb-10" :prospect="data ? data : {}" />
+    <OpportunityTable class="mb-10" :prospect="member ? member : {}" />
     <AssetsConsolidations class="mb-10" />
   </div>
 </template>
@@ -24,6 +26,7 @@ import { useProspectDetails } from '@/api/use-prospect-details.js'
 import { computed } from 'vue'
 
 import AssetsConsolidations from '@/components/NewProspect/AssetsConsolidations/AssetsConsolidations.vue'
+import WidgetOther from '@/components/MemberDetails/WidgetOther.vue'
 
 export default {
   name: 'MemberDetails',
@@ -34,22 +37,24 @@ export default {
     WidgetPastStress,
     RetirementIncomePlan,
     AssetsConsolidations,
+    WidgetOther,
   },
 
   setup() {
-    const { isLoading: isLoadingProspectDetails, isError, data, updateMemberInfo } = useProspectDetails()
+    const { isLoading: isLoadingProspectDetails, isError, data: member, updateMemberInfo, other } = useProspectDetails()
 
     const getTitle = computed(() => {
-      if (data.value && data.value.type === 'prospect') return 'Prospect details'
+      if (member.value && member.value.type === 'prospect') return 'Prospect details'
       return 'Client details'
     })
 
     return {
       isLoadingProspectDetails,
       isError,
-      data,
+      member,
       getTitle,
       updateMemberInfo,
+      other,
     }
   },
 }
