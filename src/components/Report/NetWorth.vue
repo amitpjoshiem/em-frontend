5cc6862d096ea34e0d6cf3e8c1e391d54820b059
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-11-10 20:49:45 +0200

    feature (net worth)

--- src/components/Report/NetWorth.vue
+++ src/components/Report/NetWorth.vue
@@ -2,42 +2,51 @@
   <div class="border border-color-grey rounded-lg mr-5 mt-5 p-5">
     <span class="text-main text-smm font-semibold">Net Worth</span>
     <div v-if="isFetched" class="flex mt-5">
-      <div class="w-8/12 flex justify-between">
+      <div class="w-8/12">
         <el-form ref="form" :model="ruleForm" status-icon size="mini">
           <div class="flex justify-center items-center">
             <div class="bg-dark-blue-charts w-2 h-2 rounded-full mr-1" />
-            <div class="text-main text-sm w-4/12">Liquid</div>
-            <el-form-item class="w-5/12 pr-3">
+            <div class="text-main text-xss w-4/12">Liquid</div>
+            <el-form-item class="w-5/12">
               <el-input v-model="ruleForm.liquid.amount" placeholder="$12345" type="number" @change="change()">
                 <template #prepend>$</template>
               </el-input>
             </el-form-item>
-            <div class="text-main text-sm font-semibold w-2/12">
-              {{ percentFormat(ruleForm.liquid.percent / 100) }}
+            <div class="text-main text-xss font-semibold w-2/12 pl-2">
+              <SwdSpinner v-if="isFetching" />
+              <span v-else>
+                {{ percentFormat(ruleForm.liquid.percent / 100) }}
+              </span>
             </div>
           </div>
           <div class="flex justify-center items-center my-2">
             <div class="bg-activity w-2 h-2 rounded-full mr-1" />
-            <div class="text-main text-sm w-4/12">Market</div>
-            <el-form-item class="w-5/12 pr-3">
+            <div class="text-main text-xss w-4/12">Market</div>
+            <el-form-item class="w-5/12">
               <el-input v-model="ruleForm.market.amount" placeholder="$12345" type="number" @change="change()">
                 <template #prepend>$</template>
               </el-input>
             </el-form-item>
-            <div class="text-main text-sm font-semibold w-2/12">
-              {{ percentFormat(ruleForm.market.percent / 100) }}
+            <div class="text-main text-xss font-semibold w-2/12 pl-2">
+              <SwdSpinner v-if="isFetching" />
+              <span v-else>
+                {{ percentFormat(ruleForm.market.percent / 100) }}
+              </span>
             </div>
           </div>
           <div class="flex justify-center items-center">
             <div class="bg-color-error w-2 h-2 rounded-full mr-1" />
-            <div class="text-main text-sm w-4/12">Income safe</div>
-            <el-form-item class="w-5/12 pr-3">
+            <div class="text-main text-xss w-4/12">Income safe</div>
+            <el-form-item class="w-5/12">
               <el-input v-model="ruleForm.income_safe.amount" placeholder="$12345" type="number" @change="change()">
                 <template #prepend>$</template>
               </el-input>
             </el-form-item>
-            <div class="text-main text-sm font-semibold w-2/12">
-              {{ percentFormat(ruleForm.income_safe.percent / 100) }}
+            <div class="text-main text-xss font-semibold w-2/12 pl-2">
+              <SwdSpinner v-if="isFetching" />
+              <span v-else>
+                {{ percentFormat(ruleForm.income_safe.percent / 100) }}
+              </span>
             </div>
           </div>
         </el-form>
