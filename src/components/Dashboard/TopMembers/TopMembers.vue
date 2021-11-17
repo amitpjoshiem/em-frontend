02f8ab2400b68765797dfcaf1983af9732c747db
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-11-17 11:58:09 +0200

    feature (empty user)

--- src/components/Dashboard/TopMembers/TopMembers.vue
+++ src/components/Dashboard/TopMembers/TopMembers.vue
@@ -2,6 +2,12 @@
   <div class="border-l border-r border-b rounded-b-xl border-color-grey pt-4 topmembers-block mr-5">
     <div v-if="!isLoading">
       <TopMembersItem v-for="(member, index) in members.data" :key="index" :member="member" />
+      <div v-if="!members.data.length" class="flex flex-col items-center justify-center mt-10">
+        <div class="bg-widget-bg rounded-full w-16 h-16 flex flex-col items-center justify-center mb-3">
+          <InlineSvg :src="IconEmptyUsers" />
+        </div>
+        <p class="text-gray03 font-semibold text-xss mt-3">No recently added prospects</p>
+      </div>
     </div>
     <el-skeleton v-else :rows="4" animated class="p-5" />
   </div>
