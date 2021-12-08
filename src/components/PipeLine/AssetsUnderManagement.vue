a8d8eb394f27330c8d0e2c099a875c7b1b750273
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-12-08 12:44:11 +0200

    feature (minor change text)

--- src/components/PipeLine/AssetsUnderManagement.vue
+++ src/components/PipeLine/AssetsUnderManagement.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="border border-color-grey rounded-xl p-5">
     <div v-if="!isLoading">
-      <div class="text-smm text-main font-semibold mb-5">Assets Under Managment</div>
+      <div class="text-smm text-main font-semibold mb-5">Assets Under Management</div>
       <AumChart :values="aum.data" class="my-4" />
     </div>
     <el-skeleton v-else :rows="3" animated class="p-5" />
@@ -12,7 +12,7 @@ import { usePipeLineAum } from '@/api/use-pipeline-aum.js'
 import AumChart from '@/components/PipeLine/Chart/AumChart/AumChart.vue'
 
 export default {
-  name: 'AssetsUnderManagment',
+  name: 'AssetsUnderManagement',
   components: {
     AumChart,
   },
