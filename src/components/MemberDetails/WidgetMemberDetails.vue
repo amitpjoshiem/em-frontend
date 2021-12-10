bf4e698bba77683125352a90f874fa59db92044c
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-12-10 16:27:09 +0200

    feature (Add marker to show if Account is created)

--- src/components/MemberDetails/WidgetMemberDetails.vue
+++ src/components/MemberDetails/WidgetMemberDetails.vue
@@ -1,75 +1,73 @@
 <template>
-  <div class="w-8/24 mr-5">
-    <div class="bg-widget-bg rounded-lg p-5">
-      <div class="border-b pb-5 flex justify-between">
-        <div class="flex">
-          <SwdAvatar :link="user.avatar.url" />
-          <div class="flex flex-col ml-2">
-            <router-link
-              :to="{
-                name: 'member-basic-information',
-                params: { id: memberId },
-              }"
-              class="text-sm text-main font-medium"
-            >
-              {{ user.name }}
-            </router-link>
-            <span class="text-small text-activity-item font-medium uppercase">
-              {{ user.type }}
-            </span>
-          </div>
-        </div>
-        <SwdMemberActions :user="user" />
-      </div>
-
-      <div class="flex justify-between py-3">
-        <div class="flex items-center">
-          <span class="w-6 h-6 rounded-md flex justify-center items-center bg-color-error">
-            <InlineSvg :src="IconProspectAge" />
+  <div class="bg-widget-bg rounded-lg p-5">
+    <div class="border-b pb-5 flex justify-between">
+      <div class="flex">
+        <SwdAvatar :link="user.avatar.url" />
+        <div class="flex flex-col ml-2">
+          <router-link
+            :to="{
+              name: 'member-basic-information',
+              params: { id: memberId },
+            }"
+            class="text-sm text-main font-medium"
+          >
+            {{ user.name }}
+          </router-link>
+          <span class="text-small text-activity-item font-medium uppercase">
+            {{ user.type }}
           </span>
-          <span class="ml-2 text-xs text-gray03">Age</span>
-        </div>
-        <div class="text-sm">
-          <span class="text-main pr-2">{{ user.age }}</span>
         </div>
       </div>
+      <SwdMemberActions :user="user" />
+    </div>
 
-      <div class="flex justify-between">
-        <div class="flex items-center">
-          <span class="w-6 h-6 rounded-md flex justify-center items-center bg-color-error">
-            <InlineSvg :src="IconTotal" />
-          </span>
-          <span class="ml-2 text-xs text-gray03">Total net worth:</span>
-        </div>
-        <div class="text-sm">
-          <span class="text-main font-semibold">$900,000</span>
-        </div>
+    <div class="flex justify-between py-3">
+      <div class="flex items-center">
+        <span class="w-6 h-6 rounded-md flex justify-center items-center bg-color-error">
+          <InlineSvg :src="IconProspectAge" />
+        </span>
+        <span class="ml-2 text-xs text-gray03">Age</span>
       </div>
+      <div class="text-sm">
+        <span class="text-main pr-2">{{ user.age }}</span>
+      </div>
+    </div>
 
-      <div class="flex justify-between pt-3">
-        <div class="flex items-center">
-          <span class="w-6 h-6 rounded-md flex justify-center items-center bg-color-error">
-            <InlineSvg :src="IconGoal" />
-          </span>
-          <span class="ml-2 text-xs text-gray03">Goal:</span>
-        </div>
-        <div class="text-sm">
-          <span class="text-main font-semibold">$1,200,000</span>
-        </div>
+    <div class="flex justify-between">
+      <div class="flex items-center">
+        <span class="w-6 h-6 rounded-md flex justify-center items-center bg-color-error">
+          <InlineSvg :src="IconTotal" />
+        </span>
+        <span class="ml-2 text-xs text-gray03">Total net worth:</span>
       </div>
+      <div class="text-sm">
+        <span class="text-main font-semibold">$900,000</span>
+      </div>
+    </div>
 
-      <div v-if="user.type === 'prospect'" class="flex justify-between pt-5">
-        <Button small-btn-activity text-semi-bold text-btn="Convert to client" @click="convert" />
+    <div class="flex justify-between pt-3">
+      <div class="flex items-center">
+        <span class="w-6 h-6 rounded-md flex justify-center items-center bg-color-error">
+          <InlineSvg :src="IconGoal" />
+        </span>
+        <span class="ml-2 text-xs text-gray03">Goal:</span>
+      </div>
+      <div class="text-sm">
+        <span class="text-main font-semibold">$1,200,000</span>
+      </div>
+    </div>
 
-        <router-link :to="{ name: 'member-report', params: { id: user.id } }" class="pl-2.5 font-medium">
-          <Button small-btn-gray text-semi-bold text-btn="Blueprint report" />
-        </router-link>
-      </div>
-      <div v-else class="flex justify-end pt-5">
-        <router-link :to="{ name: 'clientreport', params: { id: user.id } }" class="pl-2.5 font-medium">
-          <Button small-btn-gray text-semi-bold text-btn="Client report" />
-        </router-link>
-      </div>
+    <div v-if="user.type === 'prospect'" class="flex justify-between pt-5">
+      <Button small-btn-activity text-semi-bold text-btn="Convert to client" @click="convert" />
+
+      <router-link :to="{ name: 'member-report', params: { id: user.id } }" class="pl-2.5 font-medium">
+        <Button small-btn-gray text-semi-bold text-btn="Blueprint report" />
+      </router-link>
+    </div>
+    <div v-else class="flex justify-end pt-5">
+      <router-link :to="{ name: 'clientreport', params: { id: user.id } }" class="pl-2.5 font-medium">
+        <Button small-btn-gray text-semi-bold text-btn="Client report" />
+      </router-link>
     </div>
   </div>
 </template>
