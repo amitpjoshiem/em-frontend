9f31ead7482111bd475a9e3c0ed803b45b5baa3f
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2021-12-13 17:44:23 +0200

    feature (unit test EmploymentHistory)

--- src/components/EmploymentHistory/EmploymentHistoryItem.vue
+++ src/components/EmploymentHistory/EmploymentHistoryItem.vue
@@ -2,28 +2,28 @@
   <div class="my-3 text-xs border border-input-border rounded p-5">
     <div class="flex">
       <div class="w-4/12 text-gray03 font-semibold">Company Name</div>
-      <div class="w-4/12">
+      <div class="w-4/12" data-testid="employment-history-companyname-member">
         <SwdStubForText :text="employmentItem.companyName[0]" plug="&mdash;" />
       </div>
-      <div class="w-4/12">
+      <div class="w-4/12" data-testid="employment-history-companyname-spouse">
         <SwdStubForText :text="employmentItem.companyName[1]" plug="&mdash;" />
       </div>
     </div>
     <div class="flex py-2">
       <div class="w-4/12 text-gray03 font-semibold">Occupation</div>
-      <div class="w-4/12">
+      <div class="w-4/12" data-testid="employment-history-occupation-member">
         <SwdStubForText :text="employmentItem.occupation[0]" plug="&mdash;" />
       </div>
-      <div class="w-4/12">
+      <div class="w-4/12" data-testid="employment-history-occupation-spouse">
         <SwdStubForText :text="employmentItem.occupation[1]" plug="&mdash;" />
       </div>
     </div>
     <div class="flex">
       <div class="w-4/12 text-gray03 font-semibold">Years</div>
-      <div class="w-4/12">
+      <div class="w-4/12" data-testid="employment-history-years-member">
         <SwdStubForText :text="employmentItem.years[0]" plug="&mdash;" />
       </div>
-      <div class="w-4/12">
+      <div class="w-4/12" data-testid="employment-history-years-spouse">
         <SwdStubForText :text="employmentItem.years[1]" plug="&mdash;" />
       </div>
     </div>
@@ -31,13 +31,11 @@
 </template>
 
 <script>
-import { EmploymentHistoryMerged } from '@/utils/employmentDetail'
-
 export default {
   name: 'EmploymentHistoryItem',
   props: {
     employmentItem: {
-      type: EmploymentHistoryMerged,
+      type: Object,
       require: true,
       default: () => {},
     },
--- src/components/EmploymentHistory/__tests__/EmploymentHistoryItem.spec.js
+++ src/components/EmploymentHistory/__tests__/EmploymentHistoryItem.spec.js
@@ -1,26 +1,91 @@
 /* eslint-disable no-undef */
 import { mount } from '@vue/test-utils'
 import EmploymentHistoryItem from '@/components/EmploymentHistory/EmploymentHistoryItem.vue'
+import SwdStubForText from '@/components/Global/SwdStubForText.vue'
 
 let wrapper = null
 
 describe('EmploymentHistoryItem.vue', () => {
-  wrapper = mount(EmploymentHistoryItem, {
-    props: {
-      userType: 'prospect',
-    },
+  wrapper = null
+
+  beforeEach(() => {
+    wrapper = mount(EmploymentHistoryItem, {
+      global: {
+        components: {
+          SwdStubForText,
+        },
+      },
+      props: {
+        employmentItem: {
+          companyName: ['Uinno', 'Engineersmind'],
+          occupation: ['FE developer', 'BE developer'],
+          years: ['11', '22'],
+        },
+      },
+    })
   })
 
   test('renders EmploymentHistoryItem prospect', () => {
-    expect(wrapper.props().userType).toBe('prospect')
+    expect(wrapper.props().employmentItem).toEqual({
+      companyName: ['Uinno', 'Engineersmind'],
+      occupation: ['FE developer', 'BE developer'],
+      years: ['11', '22'],
+    })
   })
 
-  // test('renders EmploymentHistoryItem prospect type', () => {
-  //   expect(wrapper.find('div').text()).toBe('prospect')
-  // })
+  test('content testing member company name', () => {
+    expect(wrapper.find('[data-testid="employment-history-companyname-member"]').text()).toBe('Uinno')
+  })
 
-  // test('renders EmploymentHistoryItem client type', async () => {
-  //   await wrapper.setProps({ userType: 'client' })
-  //   expect(wrapper.find('div').text()).toBe('client')
+  test('content testing spouse company name', () => {
+    expect(wrapper.find('[data-testid="employment-history-companyname-spouse"]').text()).toBe('Engineersmind')
+  })
+
+  test('content testing member occupation', () => {
+    expect(wrapper.find('[data-testid="employment-history-occupation-member"]').text()).toBe('FE developer')
+  })
+
+  test('content testing member occupation', () => {
+    expect(wrapper.find('[data-testid="employment-history-occupation-spouse"]').text()).toBe('BE developer')
+  })
+
+  test('content testing member years', () => {
+    expect(wrapper.find('[data-testid="employment-history-years-member"]').text()).toBe('11')
+  })
+
+  test('content testing spouse years', () => {
+    expect(wrapper.find('[data-testid="employment-history-years-spouse"]').text()).toBe('22')
+  })
+
+  test('content testing spouse empty employment-history ', async () => {
+    await wrapper.setProps({
+      employmentItem: {
+        companyName: ['Uinno', ''],
+        occupation: ['FE developer', ''],
+        years: ['11', ''],
+      },
+    })
+
+    expect(wrapper.find('[data-testid="employment-history-companyname-spouse"]').text()).toBe('—')
+    expect(wrapper.find('[data-testid="employment-history-occupation-spouse"]').text()).toBe('—')
+    expect(wrapper.find('[data-testid="employment-history-years-spouse"]').text()).toBe('—')
+  })
+
+  test('content testing member empty employment-history ', async () => {
+    await wrapper.setProps({
+      employmentItem: {
+        companyName: ['', 'Uinno'],
+        occupation: ['', 'FE developer'],
+        years: ['', '11'],
+      },
+    })
+
+    expect(wrapper.find('[data-testid="employment-history-companyname-member"]').text()).toBe('—')
+    expect(wrapper.find('[data-testid="employment-history-occupation-member"]').text()).toBe('—')
+    expect(wrapper.find('[data-testid="employment-history-years-member"]').text()).toBe('—')
+  })
+
+  // test('content testing member stage', () => {
+  //   expect(wrapper.find('[data-testid="top-members-stage"]').text()).toBe('Closed Won')
   // })
 })
