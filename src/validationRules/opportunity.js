8bdd9566aabf34b5741848f6794e25b1640808fe
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2022-02-15 20:22:56 +0200

    feature (Remove validation fields add Opportunity)

--- src/validationRules/opportunity.js
+++ src/validationRules/opportunity.js
@@ -1,19 +1,19 @@
 const rules = {
-  stage: [
-    {
-      required: true,
-      message: 'Please select stage',
-      trigger: 'change',
-    },
-  ],
-  close_date: [
-    {
-      type: 'date',
-      required: true,
-      message: 'Please pick a date',
-      trigger: 'change',
-    },
-  ],
+  // stage: [
+  //   {
+  //     required: true,
+  //     message: 'Please select stage',
+  //     trigger: 'change',
+  //   },
+  // ],
+  // close_date: [
+  //   {
+  //     type: 'date',
+  //     required: true,
+  //     message: 'Please pick a date',
+  //     trigger: 'change',
+  //   },
+  // ],
   opportunity_name: [
     {
       required: true,
@@ -22,18 +22,17 @@ const rules = {
     },
     { min: 1, message: 'Length should be min 1', trigger: 'blur' },
   ],
-  type: [
-    {
-      required: true,
-      message: 'Please select type',
-      trigger: 'change',
-    },
-  ],
+  // type: [
+  //   {
+  //     required: true,
+  //     message: 'Please select type',
+  //     trigger: 'change',
+  //   },
+  // ],
   amount: [
     {
       validator: validateNumber,
       trigger: 'blur',
-      required: true,
     },
   ],
 }
