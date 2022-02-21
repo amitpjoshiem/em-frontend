67766c6fb01ebce19dfedce33eb92cc90d07d925
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2022-02-21 15:32:22 +0200

    feature (SF fix) - minor edit

--- src/api/use-sales-force-auth.js
+++ src/api/use-sales-force-auth.js
@@ -1,25 +1,3 @@
-// import { useFetch } from '@/api/use-fetch'
-
-// const useSalesForceAuth = () => {
-//   const { response, error, fetching, fetchData } = useFetch('/salesforce/auth/settings', {
-//     method: 'GET',
-//   })
-
-//   const getSalesForceAuth = async (body) => {
-//     await fetchData({ body })
-//     if (error.value !== null) return
-//   }
-
-//   return {
-//     response,
-//     error,
-//     fetching,
-//     getSalesForceAuth,
-//   }
-// }
-
-// export { useSalesForceAuth }
-
 import { useQuery } from 'vue-query'
 import { fetchSalesForceAuth } from './vueQuery/fetch-sales-force-auth'
 
