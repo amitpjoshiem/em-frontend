fdedfd63fe73f428af5b9681d4afd88bfea09d36
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2022-03-03 13:31:47 +0200

    feature (Opportunity Contact)

--- src/components/OpportunityContact/OpportunityContact.vue
+++ src/components/OpportunityContact/OpportunityContact.vue
@@ -5,7 +5,7 @@
       <el-button type="primary" @click="addContact">Add contact</el-button>
     </div>
     <div v-if="!isLoading">
-      <el-card v-for="item in contacts" :key="item.id" class="box-card mb-5">
+      <el-card v-for="item in state" :key="item.id" class="box-card mb-5">
         <div class="flex">
           <div class="w-2/12 flex items-center justify-center">
             <SwdAvatar size="large" />
@@ -21,7 +21,7 @@
             </div>
             <div class="mb-1">
               <span class="text-xss text-gray03 font-semibold pr-2">Birthday:</span>
-              <SwdStubForText :text="item.birthdayFormatted" plug="&mdash;" class="text-sm text-main inline-block" />
+              <SwdStubForText :text="item.birthday" plug="&mdash;" class="text-sm text-main inline-block" />
             </div>
             <div class="mb-1">
               <span class="text-xss text-gray03 font-semibold pr-2">Age:</span>
@@ -47,7 +47,7 @@
           <div class="flex flex-col w-4/12">
             <div class="mb-1">
               <span class="text-xss text-gray03 font-semibold pr-2">Retirement date:</span>
-              <SwdStubForText :text="item.retirementFormatted" plug="&mdash;" class="text-sm text-main inline-block" />
+              <SwdStubForText :text="item.retirement_date" plug="&mdash;" class="text-sm text-main inline-block" />
             </div>
             <div class="mb-1">
               <span class="text-xss text-gray03 font-semibold pr-2">Phone:</span>
@@ -55,67 +55,123 @@
             </div>
             <div class="mb-1">
               <span class="text-xss text-gray03 font-semibold pr-2">Created:</span>
-              <SwdStubForText :text="item.createdFormatted" plug="&mdash;" class="text-sm text-main inline-block" />
+              <SwdStubForText :text="item.created_at" plug="&mdash;" class="text-sm text-main inline-block" />
             </div>
             <div class="mb-1">
               <span class="text-xss text-gray03 font-semibold pr-2">Updated</span>
-              <SwdStubForText :text="item.updatedFormatted" plug="&mdash;" class="text-sm text-main inline-block" />
+              <SwdStubForText :text="item.updated_at" plug="&mdash;" class="text-sm text-main inline-block" />
             </div>
             <div>
-              <el-checkbox v-model="item.is_spouse" label="Spouse" size="small" />
+              <el-checkbox
+                v-model="item.is_spouse"
+                label="Spouse"
+                size="small"
+                :disabled="loadingUpdateContact || isFetching"
+                @change="changeSpouse(item.id)"
+              />
             </div>
           </div>
           <div class="w-1/12 flex flex-col items-center justify-center">
-            <el-button type="primary" :icon="Edit" circle @click="editContact" />
-            <el-button type="danger" :icon="Delete" circle @click="removeContact" />
+            <el-button type="primary" :icon="Edit" circle @click="editContact(item.id)" />
+            <el-popconfirm
+              confirm-button-text="OK"
+              cancel-button-text="No, Thanks"
+              :icon="InfoFilled"
+              icon-color="red"
+              title="Are you sure to delete this?"
+              @confirm="removeContact(item.id)"
+            >
+              <template #reference>
+                <el-button type="danger" :icon="Delete" circle />
+              </template>
+            </el-popconfirm>
           </div>
         </div>
       </el-card>
     </div>
+    <el-skeleton v-else :rows="10" animated />
   </div>
 </template>
 
 <script>
+import { reactive, watchEffect } from 'vue'
 import { useFetchAllContacts } from '@/api/use-fetch-all-contacts.js'
 import { useRoute } from 'vue-router'
-import { Edit, Delete } from '@element-plus/icons-vue'
+import { Edit, Delete, InfoFilled } from '@element-plus/icons-vue'
+import { updateContacts } from '@/api/vueQuery/update-contacts'
+import { useMutation, useQueryClient } from 'vue-query'
+import { useAlert } from '@/utils/use-alert'
 
 export default {
   name: 'OpportunityContact',
 
   setup() {
     const route = useRoute()
-
+    const queryClient = useQueryClient()
     const memberId = route.params.id
 
     const { isLoading, isError, isFetching, data: contacts, isFetched, refetch } = useFetchAllContacts(memberId)
+
+    const { mutateAsync: updateContact, isLoading: loadingUpdateContact } = useMutation(updateContacts)
+
+    const state = reactive({})
+
+    watchEffect(() => {
+      if (!isLoading.value) {
+        Object.assign(state, JSON.parse(JSON.stringify(contacts.value)))
+      }
+    })
 
     const addContact = () => {
       console.log('addContact')
     }
 
-    const editContact = () => {
-      console.log('editContact')
+    const editContact = (id) => {
+      console.log('editContact', id)
     }
 
-    const removeContact = () => {
-      console.log('removeContact')
+    const removeContact = (id) => {
+      console.log('removeContact', id)
+    }
+
+    const changeSpouse = async (id) => {
+      const form = {
+        is_spouse: true,
+      }
+      const res = await updateContact({ form, id })
+      responseUpdate(res)
+    }
+
+    const responseUpdate = (res) => {
+      if (!('error' in res)) {
+        useAlert({
+          title: 'Success',
+          type: 'success',
+          message: 'Update successfully',
+        })
+        queryClient.invalidateQueries(['contactsAll', memberId])
+      }
     }
 
     return {
+      state,
+
       isLoading,
       isError,
       isFetching,
       contacts,
       isFetched,
       refetch,
+      loadingUpdateContact,
 
       addContact,
       editContact,
       removeContact,
+      changeSpouse,
 
       Edit,
       Delete,
+      InfoFilled,
     }
   },
 }
