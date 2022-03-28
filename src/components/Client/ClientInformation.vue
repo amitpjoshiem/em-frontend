65074d90d6057db68496201bb2a46cb971ef2954
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2022-03-28 10:59:26 +0300

    refactoring (ClientInformation)

--- src/components/Client/ClientInformation.vue
+++ src/components/Client/ClientInformation.vue
@@ -3,10 +3,10 @@
     <el-skeleton :rows="15" animated />
   </div>
   <div v-else-if="isErrorInfo">An error has occurred: {{ error }}</div>
-  <div v-else-if="clientsInfo" class="p-5 mb-20">
+  <div v-else-if="clientsInfo" class="p-5 mb-20 text-main">
     <div class="flex flex-col items-center">
       <img src="../../assets/img/swd-logo-blue.png" alt="excel" class="w-[150px]" />
-      <div class="flex flex-col items-center text-center text-main">
+      <div class="flex flex-col items-center text-center">
         <span class="text-lg">
           In order to conducta proper analysis and make your visit productive, we ask that you providethe items listed
           below
@@ -19,56 +19,64 @@
 
     <router-link
       :to="{ name: 'client-basic-information', params: { id: clientsInfo.member_id } }"
-      class="flex items-center border border-input-border rounded-md p-5 mb-4 sm:mb-10"
+      class="flex items-center"
     >
-      <el-icon :size="25" class="mr-5">
-        <circle-check-filled :color="clientsInfo.steps.completed_financial_fact_finder ? 'green' : 'gray'" />
-      </el-icon>
-      <span class="text-xs sm:text-base">Completed Financial Fact Finder</span>
+      <el-card class="mb-4 w-full">
+        <div class="flex items-center">
+          <el-icon :size="25" class="mr-5">
+            <circle-check-filled :color="clientsInfo.steps.completed_financial_fact_finder ? '#4cd972' : 'gray'" />
+          </el-icon>
+          <div class="text-xs sm:text-base text-main">Completed Financial Fact Finder</div>
+        </div>
+      </el-card>
     </router-link>
 
-    <router-link
-      :to="{ name: 'investment-retirement' }"
-      class="flex items-center border border-input-border rounded-md p-5 mb-4 sm:mb-10"
-    >
-      <el-icon :size="25" class="mr-5">
-        <circle-check-filled :color="clientsInfo.steps.investment_and_retirement_accounts ? 'green' : 'gray'" />
-      </el-icon>
-      <span class="text-xs sm:text-base">Investment and Retirement Accounts (most recent statements)</span>
+    <router-link :to="{ name: 'investment-retirement' }" class="flex items-center">
+      <el-card class="mb-4 w-full">
+        <div class="flex items-center">
+          <el-icon :size="25" class="mr-5">
+            <circle-check-filled :color="clientsInfo.steps.investment_and_retirement_accounts ? '#4cd972' : 'gray'" />
+          </el-icon>
+          <div class="text-xs sm:text-base text-main">Investment and Retirement Accounts (most recent statements)</div>
+        </div>
+      </el-card>
     </router-link>
 
-    <router-link
-      :to="{ name: 'life-insurance' }"
-      class="flex items-center border border-input-border rounded-md p-5 mb-4 sm:mb-10"
-    >
-      <el-icon :size="25" class="mr-5">
-        <circle-check-filled
-          :color="clientsInfo.steps.life_insurance_annuity_and_long_terms_care_policies ? 'green' : 'gray'"
-        />
-      </el-icon>
-      <span class="text-xs sm:text-base"
-        >Life Insurance, Annuity, and Long Term Care Policies (most recent statements)</span
-      >
+    <router-link :to="{ name: 'life-insurance' }" class="flex items-center">
+      <el-card class="mb-4 w-full">
+        <div class="flex items-center">
+          <el-icon :size="25" class="mr-5">
+            <circle-check-filled
+              :color="clientsInfo.steps.life_insurance_annuity_and_long_terms_care_policies ? '#4cd972' : 'gray'"
+            />
+          </el-icon>
+          <div class="text-xs sm:text-base text-main">
+            Life Insurance, Annuity, and Long Term Care Policies (most recent statements)
+          </div>
+        </div>
+      </el-card>
     </router-link>
 
-    <router-link
-      :to="{ name: 'social-security' }"
-      class="flex items-center border border-input-border rounded-md p-5 mb-4 sm:mb-10"
-    >
-      <el-icon :size="25" class="mr-5">
-        <circle-check-filled :color="clientsInfo.steps.social_security_information ? 'green' : 'gray'" />
-      </el-icon>
-      <span class="text-xs sm:text-base">Social Security Information/Statement(s)</span>
+    <router-link :to="{ name: 'social-security' }" class="flex items-center">
+      <el-card class="mb-4 w-full">
+        <div class="flex items-center">
+          <el-icon :size="25" class="mr-5">
+            <circle-check-filled :color="clientsInfo.steps.social_security_information ? '#4cd972' : 'gray'" />
+          </el-icon>
+          <div class="text-xs sm:text-base text-main">Social Security Information/Statement(s)</div>
+        </div>
+      </el-card>
     </router-link>
 
-    <router-link
-      :to="{ name: 'list-stock' }"
-      class="flex items-center border border-input-border rounded-md p-5 mb-4 sm:mb-10"
-    >
-      <el-icon :size="25" class="mr-5">
-        <circle-check-filled :color="clientsInfo.steps.list_of_stock_certificates_or_bonds ? 'green' : 'gray'" />
-      </el-icon>
-      <span class="text-xs sm:text-base">List of Stock Certificates or Bonds</span>
+    <router-link :to="{ name: 'list-stock' }" class="flex">
+      <el-card class="mb-8 w-full">
+        <div class="flex items-center">
+          <el-icon :size="25" class="mr-5">
+            <circle-check-filled :color="clientsInfo.steps.list_of_stock_certificates_or_bonds ? '#4cd972' : 'gray'" />
+          </el-icon>
+          <div class="text-xs sm:text-base text-main">List of Stock Certificates or Bonds</div>
+        </div>
+      </el-card>
     </router-link>
 
     <div class="flex justify-end">
