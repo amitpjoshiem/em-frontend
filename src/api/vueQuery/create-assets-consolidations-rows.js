9b0e594510852c985c1bc8af492fe0ea1b6adc80
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2022-04-07 18:20:12 +0300

    fix (createAssetsConsolidationRows)

--- src/api/vueQuery/create-assets-consolidations-rows.js
+++ src/api/vueQuery/create-assets-consolidations-rows.js
@@ -2,7 +2,7 @@ import { fetcher } from '@/api/fetcher/fetcher'
 
 function createAssetsConsolidationRows({ memberId, tableId }) {
   return fetcher({
-    url: `/assets_consolidations/${memberId}/${tableId}`,
+    url: `/assets_consolidations/create/${memberId}/${tableId}`,
     options: { method: 'POST' },
   })
 }
