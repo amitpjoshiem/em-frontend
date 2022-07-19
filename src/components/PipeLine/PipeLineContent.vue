65ac9f2826a285d8081df686303432a5d457143b
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2022-07-19 16:03:30 +0300

    minor edit(PipeLineContent)

--- src/components/PipeLine/PipeLineContent.vue
+++ src/components/PipeLine/PipeLineContent.vue
@@ -2,15 +2,15 @@
   <div>
     <div class="flex mb-5 h-[250px]">
       <NumberLeedsContent class="w-6/12 mr-2.5" />
-      <!-- <NumberLeedsFilledDataContent class="w-6/12 ml-2.5" /> -->
+      <NumberLeedsFilledDataContent class="w-6/12 ml-2.5" />
     </div>
     <div class="flex mb-5 h-[250px]">
-      <!-- <NumberLeedsConvertedContent class="w-6/12 mr-2.5" /> -->
+      <NumberLeedsConvertedContent class="w-6/12 mr-2.5" />
       <!-- <InvestmentGrowth class="w-6/12 ml-2.5" /> -->
     </div>
   </div>
   <div class="mb-5 h-[250px]">
-    <!-- <NumberLeedsClosedContent /> -->
+    <NumberLeedsClosedContent />
     <!-- <AssetsUnderManagement class="w-6/12 mr-2.5" /> -->
   </div>
   <!-- <HouseholdsStatistics class="mb-5 h-[250px]" /> -->
@@ -22,9 +22,9 @@
 // import HouseholdsStatistics from '@/components/PipeLine//HouseholdsStatistics.vue'
 // import OpportunityList from '@/components/PipeLine/OpportunityList.vue'
 import NumberLeedsContent from '@/components/PipeLine/NumberLeedsContent.vue'
-// import NumberLeedsFilledDataContent from '@/components/PipeLine/NumberLeedsFilledDataContent.vue'
-// import NumberLeedsConvertedContent from '@/components/PipeLine/NumberLeedsConvertedContent.vue'
-// import NumberLeedsClosedContent from '@/components/PipeLine/NumberLeedsClosedContent.vue'
+import NumberLeedsFilledDataContent from '@/components/PipeLine/NumberLeedsFilledDataContent.vue'
+import NumberLeedsConvertedContent from '@/components/PipeLine/NumberLeedsConvertedContent.vue'
+import NumberLeedsClosedContent from '@/components/PipeLine/NumberLeedsClosedContent.vue'
 
 export default {
   name: 'PipeLineContent',
@@ -34,9 +34,9 @@ export default {
     // HouseholdsStatistics,
     // OpportunityList,
     NumberLeedsContent,
-    // NumberLeedsFilledDataContent,
-    // NumberLeedsConvertedContent,
-    // NumberLeedsClosedContent,
+    NumberLeedsFilledDataContent,
+    NumberLeedsConvertedContent,
+    NumberLeedsClosedContent,
   },
 }
 </script>
