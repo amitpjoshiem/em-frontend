e6ebdbf3b6cc3910a25b88f33742c6b6a4e1f47d
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2022-07-19 17:14:34 +0300

    minor edit (NumberLeeds chart)

--- src/components/PipeLine/Chart/NumberLeeds/NumberLeeds.vue
+++ src/components/PipeLine/Chart/NumberLeeds/NumberLeeds.vue
@@ -1,13 +1,19 @@
 <template>
   <div>
-    <vue3-chart-js v-bind="chartOptions" ref="chartNumberLeedsRef" />
+    <vue3-chart-js
+      :id="doughnutChart.id"
+      ref="chartNumberLeedsRef"
+      :type="doughnutChart.type"
+      :data="doughnutChart.data"
+      :options="doughnutChart.options"
+    />
   </div>
 </template>
 
 <script>
-import { ref } from 'vue'
+import { ref, computed } from 'vue'
 import Vue3ChartJs from '@j-t-mcc/vue3-chartjs'
-import { useStatisticsChartDataOptions } from '@/components/PipeLine/Chart/NumberLeeds/useDataOptions.js'
+// import { useStatisticsChartDataOptions } from '@/components/PipeLine/Chart/NumberLeeds/useDataOptions.js'
 
 export default {
   name: 'NumberLeeds',
@@ -24,11 +30,76 @@ export default {
   setup(props) {
     const chartNumberLeedsRef = ref(null)
 
-    const { chartOptions } = useStatisticsChartDataOptions(props.values)
+    // const { chartOptions } = useStatisticsChartDataOptions(props.values)
+
+    const getData = computed(() => {
+      const chartData = {
+        data: [],
+        labels: [],
+      }
+      props.values.forEach((item) => {
+        chartData.data.push(item.count)
+        chartData.labels.push(item.period)
+      })
+      return chartData
+    })
+
+    const doughnutChart = {
+      id: 'doughnut',
+      type: 'bar',
+      data: {
+        labels: getData.value.labels,
+        datasets: [
+          {
+            borderRadius: 20,
+            borderSkipped: false,
+            borderColor: '#41B883',
+            maxBarThickness: 10,
+            backgroundColor: ['#66B6FF'],
+            data: getData.value.data,
+          },
+        ],
+      },
+      options: {
+        maintainAspectRatio: false,
+        scales: {
+          y: {
+            type: 'linear',
+            ticks: {
+              min: 0,
+              font: {
+                size: 9,
+              },
+              fontFamily: 'Work Sans',
+              color: '#424450',
+            },
+            grid: {
+              borderDash: [8, 8],
+              color: '#EFF6FF',
+            },
+          },
+          x: {
+            grid: {
+              display: false,
+            },
+            ticks: {
+              color: '#424450',
+              font: {
+                size: 9,
+              },
+            },
+          },
+        },
+        plugins: {
+          legend: false,
+        },
+      },
+    }
 
     return {
       chartNumberLeedsRef,
-      chartOptions,
+      doughnutChart,
+      // chartOptions,
     }
   },
 }
