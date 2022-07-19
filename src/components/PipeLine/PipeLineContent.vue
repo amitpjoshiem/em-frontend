322b13be4c4d2cb7154e2c8ff7dc5010550e8d23
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2022-07-19 16:11:04 +0300

    minor edit (PipeLineContent)

--- src/components/PipeLine/PipeLineContent.vue
+++ src/components/PipeLine/PipeLineContent.vue
@@ -6,21 +6,21 @@
     </div>
     <div class="flex mb-5 h-[250px]">
       <NumberLeedsConvertedContent class="w-6/12 mr-2.5" />
-      <!-- <InvestmentGrowth class="w-6/12 ml-2.5" /> -->
+      <InvestmentGrowth class="w-6/12 ml-2.5" />
     </div>
   </div>
   <div class="mb-5 h-[250px]">
     <NumberLeedsClosedContent />
     <!-- <AssetsUnderManagement class="w-6/12 mr-2.5" /> -->
   </div>
-  <!-- <HouseholdsStatistics class="mb-5 h-[250px]" /> -->
-  <!-- <OpportunityList /> -->
+  <HouseholdsStatistics class="mb-5 h-[250px]" />
+  <OpportunityList />
 </template>
 <script>
 // import AssetsUnderManagement from '@/components/PipeLine/AssetsUnderManagement.vue'
-// import InvestmentGrowth from '@/components/PipeLine/InvestmentGrowth.vue'
-// import HouseholdsStatistics from '@/components/PipeLine//HouseholdsStatistics.vue'
-// import OpportunityList from '@/components/PipeLine/OpportunityList.vue'
+import InvestmentGrowth from '@/components/PipeLine/InvestmentGrowth.vue'
+import HouseholdsStatistics from '@/components/PipeLine//HouseholdsStatistics.vue'
+import OpportunityList from '@/components/PipeLine/OpportunityList.vue'
 import NumberLeedsContent from '@/components/PipeLine/NumberLeedsContent.vue'
 import NumberLeedsFilledDataContent from '@/components/PipeLine/NumberLeedsFilledDataContent.vue'
 import NumberLeedsConvertedContent from '@/components/PipeLine/NumberLeedsConvertedContent.vue'
@@ -30,9 +30,9 @@ export default {
   name: 'PipeLineContent',
   components: {
     // AssetsUnderManagement,
-    // InvestmentGrowth,
-    // HouseholdsStatistics,
-    // OpportunityList,
+    InvestmentGrowth,
+    HouseholdsStatistics,
+    OpportunityList,
     NumberLeedsContent,
     NumberLeedsFilledDataContent,
     NumberLeedsConvertedContent,
