74a2b21f37b740ad09e6cc35b3150af9deeed047
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2022-07-20 11:21:46 +0300

    feature (member steps)

--- src/components/MemberDetails/MemberStep.vue
+++ src/components/MemberDetails/MemberStep.vue
@@ -1,15 +1,92 @@
 <template>
-  <div>
-    <el-steps :active="active" simple>
-      <el-step title="1st Appointment" class="cursor-pointer" @click="setActive(1)" />
-      <el-step title="2nd Appointment" class="cursor-pointer" @click="setActive(2)" />
-      <el-step title="3rd Appointment" class="cursor-pointer" @click="setActive(3)" />
-      <el-step title="Place Holder Acct" class="cursor-pointer" @click="setActive(4)" />
-      <el-step title="Paperwork Signed" class="cursor-pointer" @click="setActive(5)" />
-      <el-step title="Commission Paid" class="cursor-pointer" @click="setActive(6)" />
-      <el-step title="Contract delivery/Free look period" class="cursor-pointer" @click="setActive(7)" />
-      <el-step title="Closed" class="cursor-pointer" @click="setActive(8)" />
-    </el-steps>
+  <div class="flex justify-between">
+    <div
+      class="signpost"
+      :class="{
+        'step-complet': active > 1,
+        'step-current': active === 1,
+      }"
+      @click="setActive(1)"
+    >
+      <p>1st Appointment</p>
+    </div>
+    <div
+      class="signpost"
+      :class="{
+        'step-complet': active > 2,
+        'step-no-complet': active < 2,
+        'step-current': active === 2,
+      }"
+      @click="setActive(2)"
+    >
+      <p>2nd Appointment</p>
+    </div>
+    <div
+      class="signpost"
+      :class="{
+        'step-complet': active > 3,
+        'step-no-complet': active < 3,
+        'step-current': active === 3,
+      }"
+      @click="setActive(3)"
+    >
+      <p>3rd Appointment</p>
+    </div>
+    <div
+      class="signpost"
+      :class="{
+        'step-complet': active > 4,
+        'step-no-complet': active < 4,
+        'step-current': active === 4,
+      }"
+      @click="setActive(4)"
+    >
+      <p>Place Holder Acct</p>
+    </div>
+    <div
+      class="signpost"
+      :class="{
+        'step-complet': active > 5,
+        'step-no-complet': active < 5,
+        'step-current': active === 5,
+      }"
+      @click="setActive(5)"
+    >
+      <p>Paperwork Signed</p>
+    </div>
+    <div
+      class="signpost"
+      :class="{
+        'step-complet': active > 6,
+        'step-no-complet': active < 6,
+        'step-current': active === 6,
+      }"
+      @click="setActive(6)"
+    >
+      <p>Commission Paid</p>
+    </div>
+    <div
+      class="signpost flex-col"
+      :class="{
+        'step-complet': active > 7,
+        'step-no-complet': active < 7,
+        'step-current': active === 7,
+      }"
+      @click="setActive(7)"
+    >
+      <p>Contract delivery/</p>
+      <p>Free look period</p>
+    </div>
+    <div
+      class="signpost"
+      :class="{
+        'step-no-complet': active < 8,
+        'step-current': active === 8,
+      }"
+      @click="setActive(8)"
+    >
+      <p>Closed</p>
+    </div>
   </div>
 </template>
 
@@ -20,10 +97,10 @@ import { useAlert } from '@/utils/use-alert'
 export default {
   name: 'MemberStep',
   setup() {
-    const active = ref(0)
+    const active = ref(1)
 
-    const setActive = (item) => {
-      active.value = item
+    const setActive = (step) => {
+      active.value = step
       useAlert({
         title: 'Success',
         type: 'success',
@@ -39,17 +116,42 @@ export default {
 }
 </script>
 
-<style>
-.el-steps--simple {
-  padding: 10px;
+<style scoped>
+.signpost {
+  cursor: pointer;
+  width: 12.5%;
+  height: 40px;
+  clip-path: polygon(0% 0%, 92% 0%, 100% 50%, 92% 100%, 0% 100%, 6% 50%);
+  shape-outside: polygon(0% 0%, 92% 0%, 100% 50%, 92% 100%, 0% 100%, 6% 50%);
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
-.el-step.is-simple .el-step__head {
-  display: none;
-}
-.el-step.is-simple .el-step__title {
+.signpost p {
   font-size: 11px;
+  text-align: center;
 }
-.el-step.is-simple:not(:last-of-type) .el-step__title {
-  max-width: 80%;
+
+.step-no-complet {
+  background-color: rgb(243, 243, 243);
+}
+
+.step-no-complet p {
+  color: #000;
+}
+
+.step-complet {
+  background-color: #6ebaff;
+}
+
+.step-complet p {
+  color: #fff;
+}
+
+.step-current {
+  background-color: #042d52;
+}
+.step-current p {
+  color: #fff;
 }
 </style>
