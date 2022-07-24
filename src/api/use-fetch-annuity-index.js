ef901c1aa81b82db0208bc6399cd114424ff3674
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2022-07-24 17:36:58 +0300

    feature (fixed annuity index) - minor edit useAnnuityIndex

--- src/api/use-fetch-annuity-index.js
+++ src/api/use-fetch-annuity-index.js
@@ -2,7 +2,7 @@ import { useQuery } from 'vue-query'
 import { fetchAnnuityIndex } from './vueQuery/fetch-annuity-index'
 
 export const useAnnuityIndex = (id) => {
-  const { isLoading, isError, isFetching, data } = useQuery(['annuityIndex'], () => {
+  const { isLoading, isError, isFetching, data } = useQuery(['annuityIndex', id], () => {
     return fetchAnnuityIndex(id)
   })
 
