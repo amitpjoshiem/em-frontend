2e9de0d6ba4801c9e9d8d39b6949aa8e41108638
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2022-11-25 02:12:49 +0200

    minor edit (ConfirmationPage)

--- src/components/Lead/Confirm/ConfirmationPage.vue
+++ src/components/Lead/Confirm/ConfirmationPage.vue
@@ -3,7 +3,7 @@
     <SwdSubHeader :title="getTitle" :with-back-btn="!isReadOnlyLead" />
 
     <div class="border border-border-blue rounded-md p-5 mb-4">
-      <div class="flex items-center">
+      <div class="flex items-center mb-4">
         <InlineSvg :src="IconDoneStep" />
         <div class="text-main text-xl font-semibold ml-2">Basic</div>
       </div>
