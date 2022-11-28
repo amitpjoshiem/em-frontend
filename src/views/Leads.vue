149fc97b800ed3b4cc182ac472f8c50e0e3f62ec
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2022-11-28 16:38:13 +0200

    minor edit

--- src/views/Leads.vue
+++ src/views/Leads.vue
@@ -1,6 +1,6 @@
 <template>
   <SwdWrapper>
-    <SwdSubHeader title="List of Leads" />
+    <SwdSubHeader title="Client Welcome Checklists" />
     <ListContent :visible-tab="['all-leads', 'active-leads', 'deactivated-leads']" destination="leads" />
   </SwdWrapper>
 </template>
