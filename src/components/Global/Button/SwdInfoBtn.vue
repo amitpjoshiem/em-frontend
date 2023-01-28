e71e6e9b062bdf2cd6f902b41d85ff506829e0e1
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2023-01-28 13:41:13 +0200

    feat (admin panel help): edited info block

--- src/components/Global/Button/SwdInfoBtn.vue
+++ src/components/Global/Button/SwdInfoBtn.vue
@@ -1,17 +1,6 @@
 <template>
   <div
-    class="
-      cursor-pointer
-      bg-white
-      rounded
-      flex
-      justify-center
-      items-center
-      border border-color-grey
-      pr-[7px]
-      pl-[6px]
-      py-[6px]
-    "
+    class="cursor-pointer bg-white rounded flex justify-center items-center border border-color-grey px-[6px] py-[6px]"
     @click="showInfo"
   >
     <el-icon>
@@ -20,14 +9,23 @@
   </div>
 
   <el-dialog v-model="dialogVisible" title="Info" width="70%">
-    <el-scrollbar height="400px" class="pr-4">
-      <div v-if="!isFetching && data">
-        <video controls style="width: 100%; height: 100%">
-          <source :src="data.url" type="video/mp4" />
-        </video>
+    <el-scrollbar v-loading="isFetching" height="500px" class="pr-[15px]">
+      <div v-if="!isFetching && data" class="text-main">
+        <div v-if="data.text" class="mb-4">
+          <div class="border rounded-md p-5">
+            {{ data.text }}
+          </div>
+        </div>
+        <div v-if="data.url" class="h-[400px]">
+          <div class="border rounded-md p-5">
+            <video controls style="width: 100%; height: 100%">
+              <source :src="data.url" type="video/mp4" />
+            </video>
+          </div>
+        </div>
       </div>
-      <SwdSpinner v-else large />
     </el-scrollbar>
+
     <template #footer>
       <span class="dialog-footer">
         <el-button @click="dialogVisible = false">Close</el-button>
