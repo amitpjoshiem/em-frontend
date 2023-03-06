50a85a8bd76f488887f0869d2e2ea95de00caad2
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2023-03-06 12:06:09 +0200

    fix (AdvisorHeader)

--- src/components/Header/RolesForHeader/AdvisorHeader.vue
+++ src/components/Header/RolesForHeader/AdvisorHeader.vue
@@ -7,7 +7,7 @@
   </div>
   <div class="w-5/12 flex justify-end">
     <template v-if="showContent.showNewLeadBtn">
-      <!-- <NewProspectBtn class="mr-4" /> -->
+      <NewProspectBtn class="mr-4" />
       <NewLeadModal />
     </template>
 
@@ -20,6 +20,7 @@
 <script>
 import NewLeadModal from '@/components/Modal/NewLeadModal.vue'
 import NewOpportunityBtn from '@/components/Header/NewOpportunityBtn.vue'
+import NewProspectBtn from '@/components/Header/NewProspectBtn.vue'
 import UserAction from '@/components/UserAction.vue'
 import SwdRemoteSearch from '@/components/Global/SwdRemoteSearch.vue'
 import HeaderNotificationsBlock from '@/components/Header/HeaderNotificationsBlock.vue'
@@ -34,6 +35,7 @@ export default {
     HeaderNotificationsBlock,
     NewLeadModal,
     NewOpportunityBtn,
+    NewProspectBtn,
   },
   setup() {
     const { showContent } = useShowContentEnv()
