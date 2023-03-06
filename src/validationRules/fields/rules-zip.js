53ec0c9a39f7d37a5caa69e575abd9d8338b02b0
Dmytro Yarmachok <dmitry.yarmachok@uinno.io>
2023-03-06 16:15:38 +0200

    refactoring (validation rules): change rules zip code

--- src/validationRules/fields/rules-zip.js
+++ src/validationRules/fields/rules-zip.js
@@ -4,5 +4,5 @@ export const rulesZip = [
     message: 'The field cannot be empty.',
     trigger: 'change',
   },
-  { min: 5, message: 'Length should be 5.', trigger: 'blur' },
+  { min: 5, message: 'Invalid ZIP code.', trigger: 'blur' },
 ]
