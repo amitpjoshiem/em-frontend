69a0bbdcff01c0ddf42bc62efb34d3e60a39ed67
sandeep-engminds <Sandeep.Seekolu@engineersmind.com>
2023-03-06 14:11:26 +0530

    IS-118

--- src/components/Header/RolesForHeader/AdvisorHeader.vue
+++ src/components/Header/RolesForHeader/AdvisorHeader.vue
@@ -7,7 +7,7 @@
   </div>
   <div class="w-5/12 flex justify-end">
     <template v-if="showContent.showNewLeadBtn">
-      <NewProspectBtn class="mr-4" />
+      <!-- <NewProspectBtn class="mr-4" /> -->
       <NewLeadModal />
     </template>
 
@@ -19,7 +19,6 @@
 
 <script>
 import NewLeadModal from '@/components/Modal/NewLeadModal.vue'
-import NewProspectBtn from '@/components/Header/NewProspectBtn.vue'
 import NewOpportunityBtn from '@/components/Header/NewOpportunityBtn.vue'
 import UserAction from '@/components/UserAction.vue'
 import SwdRemoteSearch from '@/components/Global/SwdRemoteSearch.vue'
@@ -34,7 +33,6 @@ export default {
     SwdRemoteSearch,
     HeaderNotificationsBlock,
     NewLeadModal,
-    NewProspectBtn,
     NewOpportunityBtn,
   },
   setup() {
