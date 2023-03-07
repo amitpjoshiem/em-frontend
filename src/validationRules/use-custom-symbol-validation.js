31c40388608657c674693f3c11d701213ccca35b
Amit Joshi <123377333+amitpjoshiem@users.noreply.github.com>
2023-03-07 21:05:31 +0530

    Update src/validationRules/use-custom-symbol-validation.js

--- src/validationRules/use-custom-symbol-validation.js
+++ src/validationRules/use-custom-symbol-validation.js
@@ -1,18 +1,13 @@
-/* eslint-disable no-useless-escape */
 function allowApostrophe(rule, value, callback) {
-    
-    if (/[^\w|\/,'\(\)\-|\s]/g.test(value)) {
-        callback(new Error('Unsupported symbol'))
-      }
-    
-      if (rule.required && !value) {
-        callback(new Error(rule.errorText))
-      }
-    
-      callback()
-    }
-  
-  
-  
-  export { allowApostrophe }
-  
+  if (/[^\w|\/,'\(\)\-|\s]/g.test(value)) {
+    callback(new Error('Unsupported symbol'))
+  }
+
+  if (rule.required && !value) {
+    callback(new Error(rule.errorText))
+  }
+
+  callback()
+}
+
+export { allowApostrophe }
